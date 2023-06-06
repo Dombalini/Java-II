bb857eb7bab008b3afa61f6385e3918fdaad8270
Dominik Zovko <dentechdominik@gmail.com>
2023-06-06 12:28:30 +0200

    Upload part 15, 16 and 17

---
+++ part10-Part10_17.SortThemCards/src/main/java/BySuitInValueOrder.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tarza
+ */
+import java.util.Comparator;
+
+public class BySuitInValueOrder implements Comparator<Card> {
+	public int compare(Card c1, Card c2) {
+		if (c1.getSuit().ordinal() == c2.getSuit().ordinal()) {
+//			if (c1.getValue() == 2 && c2.getValue() != 2) {
+//				return 15 - c2.getValue();
+//			} else if (c2.getValue() == 2 && c1.getValue() != 2) {
+//				return c1.getValue() - 15;
+//			}
+			return c1.getValue() - c2.getValue();
+		}
+
+		return c1.getSuit().ordinal() - c2.getSuit().ordinal();
+	}
+}
